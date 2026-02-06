91e0f7733fd5e959dee7fcd63da352833157566c
email <prashantbansal6378@gmail.com>
2026-02-06 13:16:01 +0530

    imageP

--- frontend/src/utils/asset.js
+++ frontend/src/utils/asset.js
@@ -1,9 +1,10 @@
-export const asset = (path) => {
-  const base = import.meta.env.BASE_URL.replace(/\/$/, '')
-  const clean = path.startsWith('/') ? path : '/' + path
-  return `${base}${clean}`
-}
+// export const asset = (path) => {
+//   const base = import.meta.env.BASE_URL.replace(/\/$/, '')
+//   const clean = path.startsWith('/') ? path : '/' + path
+//   return `${base}${clean}`
+// }
 
+export const asset = (path) => path;
 
 
 
